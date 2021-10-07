087f694653a7fbacacaeab43e1d1c601926bb7cf
husainyusuf382@gmail.com <husainyusuf382@gmail.com>
2021-10-07 17:19:03 +0700

    Initial Commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float thrust = 0.5f;
+    public Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, Random.Range(0, 360), transform.eulerAngles.z);
+        Vector3 force = transform.forward;
+        force = new Vector3(force.x, 1, force.z);
+
+        rb.AddForce(force * thrust, ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
