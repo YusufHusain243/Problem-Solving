4926627b9e1053e1253a074016cef0a3edcb74dc
husainyusuf382@gmail.com <husainyusuf382@gmail.com>
2021-10-07 18:16:09 +0700

    Initial Commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,24 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public float minX, maxX, minY, maxY;
+    Vector2 pos;
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        pos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horiz = Input.GetAxis("Horizontal");
-        float vertic = Input.GetAxis("Vertical");
+        pos = Input.mousePosition;
+        pos = Camera.main.ScreenToWorldPoint(pos);
 
-        Vector2 direction = new Vector2(horiz, vertic);
-        transform.Translate(direction * speed * Time.deltaTime);
+        if (pos.x > minX && pos.x < maxX && pos.y > minY && pos.y < maxY)
+        {
+            transform.position = Vector3.Lerp(transform.position, pos, speed * Time.deltaTime);
+        }
+        else
+        {
+            return;
+        }
+        
     }
 }
